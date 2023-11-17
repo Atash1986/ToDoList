2147c885a2dc29d7330f28b73513711e012a23f2
Atash1986 <msc.ashourzadeh@gmail.com>
2023-11-17 10:12:50 +0100

    change color them

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,8 @@
 const categories = [
-  { id: 1, title: "Home Task", color: "red" },
+  { id: 1, title: "Home Task", color: "#EF476F" },
 
-  { id: 2, title: "Out Task", color: "blue" },
+  { id: 2, title: "Out Task", color: "#118AB2" },
 
-  { id: 3, title: "PC Needed", color: "green" },
+  { id: 3, title: "PC Needed", color: "#06D6A0" },
 ];
 export default categories;
