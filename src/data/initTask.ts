d39276a878462b072e5f7494370ff600336c7cb6
Atash1986 <msc.ashourzadeh@gmail.com>
2023-12-04 13:53:18 +0100

    add init task data

---
+++ src/data/initTask.ts
@@ -0,0 +1,10 @@
+import { TaskItem } from "../types/TaskItem";
+
+export const initTask: TaskItem = {
+  title: "",
+  id: -1,
+  dateAndTime: { date: "2023", time: "14:30" },
+  isDone: false,
+  authorId: -1,
+  categoryId: 0,
+};
