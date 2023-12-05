2b88113d5d31282bd6c2ba8ff31a01c50c8db0c2
Atash1986 <msc.ashourzadeh@gmail.com>
2023-12-05 13:06:35 +0100

    move icon provider to App.tsx

--- src/Component/Sidbar.tsx
+++ src/Component/Sidbar.tsx
@@ -1,6 +1,6 @@
 import React, { MouseEvent } from "react";
 import { GrDocumentText } from "react-icons/gr";
-import { IconContext } from "react-icons";
+
 import { IoSettingsOutline } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
 import logoDefaultImg from "../assest/image/LDI.png";
@@ -35,51 +35,47 @@ function Sidbar({
   }
 
   return (
-    <IconContext.Provider value={{ color: "white" }}>
-      <div className="sidbar">
-        <header>
-          <h1 className="title">{appTitle}</h1>
-          <div className="logo">{logo}</div>
-          {/* <img className="logo" src={logo} /> */}
-        </header>
-        <div className="menu">
-          <h1>To Do </h1>
+    <div className="sidbar">
+      <header>
+        <h1 className="title">{appTitle}</h1>
+        <div className="logo">{logo}</div>
+        {/* <img className="logo" src={logo} /> */}
+      </header>
+      <div className="menu">
+        <h1>To Do </h1>
 
-          <ul>
-            <CategoryItem
-              id={0}
-              color="rgba(0,0,0,0)"
-              title="All Tasks"
-              isActive={activeCategoryId === 0 ? true : false}
-              onClick={(event: MouseEvent<HTMLElement>) =>
-                handelClick(event, 0)
-              }
-            />
-            {categories.map((categoryItem: Category) => {
-              return (
-                <CategoryItem
-                  key={categoryItem.id}
-                  id={categoryItem.id}
-                  color={categoryItem.color}
-                  title={categoryItem.title}
-                  isActive={categoryItem.id === activeCategoryId ? true : false}
-                  onClick={(event: MouseEvent<HTMLElement>) =>
-                    handelClick(event, categoryItem.id)
-                  }
-                />
-              );
-            })}
-          </ul>
-        </div>
-        <footer>
-          {/* <IoDocumentTextOutline /> */}
-          <CgProfile className="profileLogo" />
-          {/* <img className="profileLogo" src={profileLogo} /> */}
-          {/* <img className="settingLogo" src={settingLogo} /> */}
-          <IoSettingsOutline className="settingLogo" />
-        </footer>
+        <ul>
+          <CategoryItem
+            id={0}
+            color="rgba(0,0,0,0)"
+            title="All Tasks"
+            isActive={activeCategoryId === 0 ? true : false}
+            onClick={(event: MouseEvent<HTMLElement>) => handelClick(event, 0)}
+          />
+          {categories.map((categoryItem: Category) => {
+            return (
+              <CategoryItem
+                key={categoryItem.id}
+                id={categoryItem.id}
+                color={categoryItem.color}
+                title={categoryItem.title}
+                isActive={categoryItem.id === activeCategoryId ? true : false}
+                onClick={(event: MouseEvent<HTMLElement>) =>
+                  handelClick(event, categoryItem.id)
+                }
+              />
+            );
+          })}
+        </ul>
       </div>
-    </IconContext.Provider>
+      <footer>
+        {/* <IoDocumentTextOutline /> */}
+        <CgProfile className="profileLogo" />
+        {/* <img className="profileLogo" src={profileLogo} /> */}
+        {/* <img className="settingLogo" src={settingLogo} /> */}
+        <IoSettingsOutline className="settingLogo" />
+      </footer>
+    </div>
   );
 }
 
