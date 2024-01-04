344d3097e5151bb07e8f9932f5b86ea93ebb5ba7
Atash1986 <msc.ashourzadeh@gmail.com>
2024-01-04 13:53:47 +0100

    correct type author

--- src/types/TaskItem.ts
+++ src/types/TaskItem.ts
@@ -4,7 +4,7 @@ export type TaskItem = {
   id: string;
   title: string;
   isDone: boolean;
-  authorId: number;
+  author: { id: number; name: string; fullName: string };
   creationDate: number;
   categoryItem: { id: number; color: string };
 };
