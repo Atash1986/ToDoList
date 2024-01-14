d88558c315c01bb94699a263e4d21ed8e053d348
Atash1986 <msc.ashourzadeh@gmail.com>
2024-01-14 20:17:55 +0100

    improve test to cover multiple samples

--- src/util/dateHelpers.test.ts
+++ src/util/dateHelpers.test.ts
@@ -1,7 +1,15 @@
 import { getDateTimeFromTimeStamp } from "./dateHelpers";
 
 it("getDateTimeFromTimeStamp() should return proper result", () => {
-  const sampleTimestamp = 1123736034;
-  const result = getDateTimeFromTimeStamp(sampleTimestamp);
-  expect(result).toEqual({ date: "Thu,11 Aug", time: "6:53" });
+  const sampleTimestamps = [1123736034, 987345765, 0];
+  const expectedResults = [
+    { date: "Thu,11 Aug", time: "6:53" },
+    { date: "Sun,15 Apr", time: "16:42" },
+    { date: "Thu,1 Jan", time: "1:0" },
+  ];
+
+  sampleTimestamps.forEach((sampleTimestamp, index) => {
+    const result = getDateTimeFromTimeStamp(sampleTimestamp);
+    expect(result).toEqual(expectedResults[index]);
+  });
 });
