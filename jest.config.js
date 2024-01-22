00964eabfcc29fde6e52b93fbb2f1cfa89357496
Atash1986 <msc.ashourzadeh@gmail.com>
2024-01-22 09:34:17 +0100

    configuration tect

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
   extensionsToTreatAsEsm: [".ts"],
   bail: 1,
   verbose: true,
+
+  moduleNameMapper: {
+    "\\.(css|less|scss|png)$": "<rootDir>/__mocks__/styleMock.js",
+  },
 };
