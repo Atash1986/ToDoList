ef406373f10758b543cf4446dc3d5be559a6ec48
Atash1986 <msc.ashourzadeh@gmail.com>
2024-01-22 09:46:12 +0100

    remove react import and clear comment

--- src/Component/App.test.tsx
+++ src/Component/App.test.tsx
@@ -1,28 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-
 import App from "./App";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from "react";
 
 test("check container div item is in page", () => {
   render(<App />);
   const s = screen.getByTestId(/app-container/i);
   expect(s).toBeInTheDocument();
 });
-
-// test("Sidbar component is present within App component", () => {
-//   render(<App />);
-
-//   const SidbarElement = screen.getByTestId("Sidbar");
-
-//   expect(SidbarElement).toBeInTheDocument();
-// });
-
-// test("MainPage component is present within App component", () => {
-//   render(<App />);
-
-//   const SidbarElement = screen.getByTestId("MainPage");
-
-//   expect(SidbarElement).toBeInTheDocument();
-// });
