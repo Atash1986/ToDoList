3416f858418c17f8105aefbb24d5a43d4f1d8c5b
Atash1986 <msc.ashourzadeh@gmail.com>
2024-01-25 11:28:18 +0100

    test for should enable or disable  the fields in case of  active category

--- src/Component/Addbox.test.tsx
+++ src/Component/Addbox.test.tsx
@@ -67,3 +67,37 @@ test("error box should be in page", () => {
   const s = screen.getByTestId(/add-box-error-box/i);
   expect(s).toBeInTheDocument();
 });
+
+test("should disable the fields in case of all task has active category id equal 0", () => {
+  render(
+    <Addbox
+      activeCategoryId={0}
+      itemId={1}
+      setItemId={() => {}}
+      setItems={() => {}}
+    />
+  );
+  const title = screen.getByTestId(/add-box-title/i);
+  expect(title).toBeDisabled();
+  const author = screen.getByTestId(/add-box-author/i);
+  expect(author).toBeDisabled();
+  const addbtn = screen.getByTestId(/add-box-add-button/i);
+  expect(addbtn).toBeDisabled();
+});
+
+test("should enable the fields in case of valid active category", () => {
+  render(
+    <Addbox
+      activeCategoryId={1}
+      itemId={1}
+      setItemId={() => {}}
+      setItems={() => {}}
+    />
+  );
+  const title = screen.getByTestId(/add-box-title/i);
+  expect(title).toBeEnabled();
+  const author = screen.getByTestId(/add-box-author/i);
+  expect(author).toBeEnabled();
+  const addbtn = screen.getByTestId(/add-box-add-button/i);
+  expect(addbtn).toBeEnabled();
+});
