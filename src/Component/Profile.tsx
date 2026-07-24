8289aef9f27bdabb3d489f04735981eadb6f51b8
Atash1986 <msc.ashourzadeh@gmail.com>
2026-07-24 10:05:26 +0200

    make profile component and logout button

--- src/Component/Profile.tsx
+++ src/Component/Profile.tsx
@@ -1,8 +1,42 @@
+import { useNavigate } from "react-router-dom";
+import "./Profile.scss";
 function Profile() {
+  const localUser = localStorage.getItem("localUser");
+const user=localUser ? JSON.parse(localUser) : null;
+ function onLogoutClick()
+ {
+   localStorage.removeItem("localUser");
+   navigate("/Login");
+ }
+ const navigate = useNavigate();
   return (
     <div style={{ padding: "20px" }}>
-      <h1> صفحه پروفایل</h1>
-    </div>
+       <h1>User Profile</h1>
+    <div className="profileBox" >
+      <p>{"Username:"} <b>{user?.userName || "Guest"}</b></p>
+      <p>{"Sign-up date: "}<b> {user?.registerDate ? new Date(Number(user.registerDate)).toLocaleDateString("en-GB", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+      }) : ""}</b></p>
+      <p>{"Last login date: "}<b>{user?.lastLoginDate ? new Date(user.lastLoginDate).toLocaleDateString("en-GB", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+      }) : "Not yet logged in"}</b></p>
+      </div>
+      <div className="logoutBtnContainer" style={{}}>
+        <button
+          className="LogoutBtn"
+          onClick={() => {
+            onLogoutClick() 
+          //   toast.success("Settings saved successfully!");
+           }}
+        >
+          LogOut
+        </button>
+        </div>
+        </div>
   );
 }
 
