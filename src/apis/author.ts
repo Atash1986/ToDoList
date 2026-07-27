ff129294ff08c45f18cfb3f51c9ab2714873b394
Atash1986 <msc.ashourzadeh@gmail.com>
2026-07-27 13:08:00 +0200

    use getapi

--- src/apis/author.ts
+++ src/apis/author.ts
@@ -1,14 +1,12 @@
-import axios from "axios";
+
 import { Authors } from "../types/Authors";
-import { baseUrl } from "./core";
+import {  getApi } from "./core";
 
 export const getAuthorsItems = async () => {
-  try {
-    const result = await axios.get(baseUrl + "authors");
-    const authorsItems: Authors[] | undefined = result.data.data;
+  const url = "authors";
+ const result = await getApi(url);
+  
+    const authorsItems: Authors[] | undefined = result
     return authorsItems;
-  } catch (error) {
-    const typedError = error as Error;
-    console.error("Error:", typedError.message);
-  }
+  
 };
