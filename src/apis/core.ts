32e4b69147fde545dd249969766839aaf984617b
Atash1986 <msc.ashourzadeh@gmail.com>
2026-07-28 12:28:07 +0200

    add withAuth to getApi

--- src/apis/core.ts
+++ src/apis/core.ts
@@ -1,16 +1,21 @@
 import axios from "axios";
-import { LOCAL_TOKEN } from "../Constants/constants";
+import { TOKEN_KEY } from "../Constants/constants";
 
 export const baseUrl = process.env.REACT_APP_API_BASE_URL || "";
 
-const token = localStorage.getItem(LOCAL_TOKEN) || "";
+const token = localStorage.getItem(TOKEN_KEY) || "";
 
-export async function getApi(url: string) {
+export async function getApi(
+  url: string,
+  withAuth: boolean = true,
+) {
   try {
     const result = await axios.get(baseUrl + url, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+      headers: withAuth
+        ? {
+          Authorization: `Bearer ${token}`,
+        }
+        : {},
     });
     return result.data.data;
   } catch (error) {
@@ -28,8 +33,8 @@ export async function postApi(
     const result = await axios.post(baseUrl + url, body, {
       headers: withAuth
         ? {
-            Authorization: `Bearer ${token}`,
-          }
+          Authorization: `Bearer ${token}`,
+        }
         : {},
     });
     return result.data;
